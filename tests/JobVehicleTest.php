ccf2c11320be0de036edf0395f119f850f91c26d
chesnutcase <chester8991@gmail.com>
2018-12-19 09:37:32 +0800

    add assignTo test

---
+++ tests/JobVehicleTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Detrack\DetrackCore\Resource\Job;
+use Detrack\DetrackCore\Resource\Vehicle;
+
+/**
+ * Tests the coupling between Job and Vehicle.
+ */
+final class JobVehicleTest extends TestCase
+{
+    public function testAssign()
+    {
+        $testJob = new Job();
+        $seed = rand();
+        $testJob->do_number = 'DCPHPv2_'.$seed;
+        $testJob->address = 'PHP Islet';
+        $testJob->date = ('Y-m-d');
+        $testJob->save();
+
+        $testVehicle = new Vehicle();
+        $testVehicle->name = 'PHP Boat';
+        $testVehicle->detrack_id = getenv('TEST_DRIVER_ID');
+        $testVehicle->save();
+
+        $testJob->assignTo($testVehicle);
+
+        $this->assertEquals($testVehicle->name, $testJob->assign_to);
+    }
+}
