18c56dc5429f5efcc818a5807405318a5d911ec4
chesnutcase <chester8991@gmail.com>
2019-01-02 17:48:53 +0800

    add docblocks for arbitary freetext fields for Item

--- src/Model/Item.php
+++ src/Model/Item.php
@@ -4,6 +4,35 @@ namespace Detrack\DetrackCore\Model;
 
 use Detrack\DetrackCore\Resource\Resource;
 
+/**
+ * Represents a line item belonging to each Job.
+ *
+ * While this class extends \Detrack\DetrackCore\Resource\Resource, it is filed under the \Detrack\DetrackCore\Model namespace
+ * because there are no special endpoints for items in the Detrack API. This class is made for formalities and convenience
+ * when abstracting items in a \Detrack\DetrackCore\Resource\Job object.
+ *
+ * @property string   $sku                   stock keeping unit. required.
+ * @property string   $description           description of item. required if `$sku` is blank
+ * @property string   $purchase_order_number arbitary field, must be enabled in Job Settings to show up in the Detrack Dashboard
+ * @property string   $batch_number          arbitary field, must be enabled in Job Settings to show up in the Detrack Dashboard
+ * @property string   $expiry_date           arbitary field, must be enabled in Job Settings to show up in the Detrack Dashboard
+ * @property string   $comments              arbitary field, must be enabled in Job Settings to show up in the Detrack Dashboard
+ * @property int      $quantity              quantity of item. will be shown on Detrack Proof of Delivery app.
+ * @property string   $unit_of_measure       arbitary field, must be enabled in Job Settings to show up in the Detrack Dashboard
+ * @property bool     $checked               arbitary field, must be enabled in Job Settings to show up in the Detrack Dashboard
+ * @property int      $actual_quantity
+ * @property int      $inbound_quantity
+ * @property string   $unload_time_estimate
+ * @property string   $unload_time_actual
+ * @property int      $follow_up_quantity
+ * @property string   $follow_up_reason
+ * @property int      $rework_quantity
+ * @property string   $rework_reason
+ * @property int      $reject_quantity
+ * @property string   $reject_reason
+ * @property float    $weight                arbitary field, must be enabled in Job Settings to show up in the Detrack Dashboard
+ * @property string[] $serial_numbers
+ */
 class Item extends Resource
 {
     /**
